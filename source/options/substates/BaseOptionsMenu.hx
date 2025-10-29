edff064a1b7d848b7ef0e1a16f79009a55106e6b
Cream.BR <creambr6@gmail.com>
2025-10-28 21:23:15 -0300

    Add mobile instance variable to BaseOptionsMenu

--- source/options/substates/BaseOptionsMenu.hx
+++ source/options/substates/BaseOptionsMenu.hx
@@ -33,6 +33,11 @@ class BaseOptionsMenu extends MusicBeatSubstate
 	public var title:String;
 	public var rpcTitle:String;
 
+	#if mobile
+	//Mobile Shit
+	public static var instance:BaseOptionsMenu;
+	#end
+
 	var titleText:FlxText;
 
 	function openSelectedSubstate(label:String)
