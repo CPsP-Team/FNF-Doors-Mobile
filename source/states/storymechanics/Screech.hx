241844df1bb2d9eed39dfe91e6d4f51f931646a8
Cream.BR <creambr6@gmail.com>
2025-10-29 23:00:17 -0300

    Update Screech.hx

--- source/states/storymechanics/Screech.hx
+++ source/states/storymechanics/Screech.hx
@@ -130,7 +130,7 @@ class Screech extends BaseSMMechanic {
 	}
 	
 	private function handleCanDodgeState() {
-		if (FlxG.keys.justPressed.SPACE || PlayState.instance.mobileControls.hitbox.buttonAction.justPressed) {
+		if (FlxG.keys.justPressed.SPACE || (FlxG.mouse.overlaps(spacebarPrompt) && FlxG.mouse.justPressed)) {
 			currentState = Dodged;
 			screechLeave.visible = true;
 			screechLeave.animation.play('stop', true, false, 6);
